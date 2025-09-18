1c6d3309842ee9ce729b33b1e336b5e7bc8dd13e
kaiy904 <D1390252@o365.fcu.edu.tw>
2025-09-18 20:39:41 +0800

    首次提交

---
+++ week2.py
@@ -0,0 +1,12 @@
+
+a = float(input("請輸入第一個數字: "))
+b = float(input("請輸入第二個數字: "))
+print("加法結果:",a+b)
+print("減法結果:",a-b)
+print("乘法結果:",a*b)
+
+if b != 0:
+    print("除法結果:", a/b)
+else:
+    print("除法結果: 無法除以零")
+
